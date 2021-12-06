9ec80a0bb0e5f8eb7572852a170a5107b492654b
Jennifer Roques <jenniferroques@gmail.com>
2021-12-06 16:25:49 -0600

    commit tree afb21ec98444f8c35634ebd3812613eadd5e455a

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
+
+const dodger = document.getElementById("dodger");
+
 function moveDodgerLeft() {
-    const leftNumbers = dodger.style.left.replace("px", "");
-    const left = parseInt(leftNumbers, 10);
-  
-    if (left > 0) {
-      dodger.style.left = `${left - 1}px`;
-    }
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
   }
-  
+}
+
 document.addEventListener("keydown", function (e) {
   if (e.key === "ArrowLeft") {
     moveDodgerLeft();
@@ -16,16 +19,16 @@ document.addEventListener("keydown", function (e) {
 
 
 function moveDodgerRight() {
-    const leftNumbers = dodger.style.left.replace("px", "");
-    const left = parseInt(leftNumbers, 10);
-  
-    if (left > 0) {
-      dodger.style.left = `${left + 1}px`;
-    }
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left < 360) {
+    dodger.style.left = `${left + 1}px`;
   }
+}
 
 document.addEventListener("keydown", function (e) {
-  if (e.key === "ArrowLeft") {
-    moveDodgerLeft();
+  if (e.key === "ArrowRight") {
+    moveDodgerright();
   }
 });
